09dc9adff47411b67c62c42965b6d98cea0927cf
Severi <severitikkanen@gmail.com>
2020-06-28 16:20:30 +0300

    Hide title when clicked outside or not in focus

--- components/index/note-bar.jsx
+++ components/index/note-bar.jsx
@@ -1,24 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import ResizableTextarea from 'components/shared/resizable-textarea'
+import OutsideClickHandler from 'react-outside-click-handler'
 
 export default function NoteBar() {
+  const [showTitle, setShowTitle] = useState(false)
+
   return (
     <div className="container">
-      <input
-        id="note-title"
-        name="note-title"
-        placeholder="Title"
-        type="text"
-      />
-      <ResizableTextarea
-        id="note-content"
-        name="note-content"
-        maxRows={5}
-        minRows={2}
-        placeholder="Remind me to..."
-        type="text"
-      />
+      <OutsideClickHandler
+        onOutsideClick={() => setShowTitle(false)}
+      >
+        <div className="wrapper">
+          {showTitle && (
+            <input
+              id="note-title"
+              name="note-title"
+              placeholder="Title"
+              type="text"
+            />
+          )}
+          <ResizableTextarea
+            id="note-content"
+            name="note-content"
+            maxRows={5}
+            minRows={1}
+            onFocus={() => setShowTitle(true)}
+            placeholder="Remind me to..."
+            type="text"
+          />
+        </div>
+      </OutsideClickHandler>
       <style jsx>{`
         .container {
           display: flex;
@@ -27,6 +39,10 @@ export default function NoteBar() {
           max-width: 400px;
           border: 1px solid;
           border-radius: 0.5rem;
+        }
+        .wrapper {
+          display: flex;
+          flex-direction: column
         }
         input {
           all: unset;
