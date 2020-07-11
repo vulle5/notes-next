c1cc457d8719a5d99d1622c85e61a21aa5cd9c07
Severi <severitikkanen@gmail.com>
2020-07-11 16:06:18 +0300

    Add git hooks to check for eslint errors

--- components/index/note.jsx
+++ components/index/note.jsx
@@ -1,31 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Note = ({ note }) => {
-  return (
-    <div className="container">
-      {note.title && <h4 className="title">{note.title}</h4>}
-      {note.content && <p>{note.content}</p>}
-      <style jsx>{`
-        .container {
-          display: flex;
-          flex-wrap: wrap;
-          max-height: 400px;
-          width: 250px;
-          padding: 0.5rem;
-          border: 1px solid ${note.color ?? 'black'};
-          border-radius: 0.5rem;
-          margin: 0.5rem;
-        }
-        .title {
-          color: red;
-          margin-bottom: 0.5rem;
-        }
-      `}
-      </style>
-    </div>
-  )
-}
+const Note = ({ note }) => (
+  <div className="container">
+    {note.title && <h4 className="title">{note.title}</h4>}
+    {note.content && <p>{note.content}</p>}
+    <style jsx>{`
+      .container {
+        display: flex;
+        flex-wrap: wrap;
+        max-height: 400px;
+        width: 250px;
+        padding: 0.5rem;
+        border: 1px solid ${note.color ?? 'black'};
+        border-radius: 0.5rem;
+        margin: 0.5rem;
+      }
+      .title {
+        color: red;
+        margin-bottom: 0.5rem;
+      }
+    `}
+    </style>
+  </div>
+)
 
 Note.defaultProps = {
   note: {}
