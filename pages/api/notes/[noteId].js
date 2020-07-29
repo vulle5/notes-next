63bc0e5fa922350f8ccdad888a4a85996efe8afe
Severi <severitikkanen@gmail.com>
2020-07-29 23:09:34 +0300

    note show endpoint

--- pages/api/notes/[noteId].js
+++ pages/api/notes/[noteId].js
@@ -4,12 +4,31 @@ export default async (req, res) => {
   const { method } = req
 
   switch (method) {
+    case 'GET':
+      await show(req, res)
+      break
     case 'DELETE':
       await destroy(req, res)
       break
     default:
       res.status(404).json({ message: '404 Not Found' })
       break
+  }
+}
+
+const show = async (req, res) => {
+  const { query } = req
+
+  try {
+    const doc = await firestore.collection('notes').doc(query.noteId).get()
+    if (doc.exists) {
+      const { createdAt, ...rest } = doc.data()
+      res.json({ id: doc.id, createdAt: createdAt.toDate(), ...rest })
+    } else {
+      res.status(404).json({ message: 'Note does not exist' })
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Internal server error 500' })
   }
 }
 
