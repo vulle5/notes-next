d772e0606bd97a4414af40902e88d59bccdc9dfb
Severi <severitikkanen@gmail.com>
2020-07-30 15:05:49 +0300

    Upgraded next to 9.5

--- styles/icons.js
+++ styles/icons.js
@@ -1,9 +1,11 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
 
-export default () => (
+const icons = () => (
   library.add(
     faCheckSquare,
     faCoffee
   )
 )
+
+export default icons
