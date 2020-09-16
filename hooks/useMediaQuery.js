46d4c9d0165bd0a66448886e9b95fcc9c62c6dc6
Severi <severitikkanen@gmail.com>
2020-09-16 20:18:47 +0300

    Fixed hydration error caused by useMediaQuery

--- hooks/useMediaQuery.js
+++ hooks/useMediaQuery.js
@@ -1,12 +1,15 @@
 import { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 
+// Ssr version of the hook. Returns null on first render
 const useMediaQuery = mediaQuery => {
-  const [value, setValue] = useState(process.browser && window.matchMedia(mediaQuery).matches)
+  const [value, setValue] = useState(null)
 
   useEffect(() => {
     const mediaQueryList = window.matchMedia(mediaQuery)
     const onEvent = ({ matches }) => setValue(matches)
+    // Set initial value on the client
+    setValue(mediaQueryList.matches)
 
     mediaQueryList.addEventListener('change', e => onEvent(e))
 
